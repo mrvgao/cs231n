75fd214f43811084ec08a2c58dbacaf19a0acff0
fortymiles <mqgao@outlook.com>
2017-04-03 18:05:10 +0800

    finish momentum sgd, it is faster convergance than sgd

--- assignment2/cs231n/optim.py
+++ assignment2/cs231n/optim.py
@@ -59,13 +59,15 @@ def sgd_momentum(w, dw, config=None):
   config.setdefault('learning_rate', 1e-2)
   config.setdefault('momentum', 0.9)
   v = config.get('velocity', np.zeros_like(w))
-  
-  next_w = None
+
+  v = config['momentum'] * v - config['learning_rate'] * dw
+
+  next_w = w + v
   #############################################################################
   # TODO: Implement the momentum update formula. Store the updated value in   #
   # the next_w variable. You should also use and update the velocity v.       #
   #############################################################################
-  pass
+
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
